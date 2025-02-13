10ef8dd9d32f4ef12c8a64f9d226a960c2ca4489
Sahitya Shankar <146736193+yraretil@users.noreply.github.com>
2025-02-13 14:03:28 +0530

    Update layout.tsx
    sdsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Head from 'next/head';
 import { useState, useEffect } from "react";
 import "./globals.css";
 
@@ -25,25 +24,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   
     return () => mediaQuery.removeEventListener("change", checkOrientation);
   }, []);
-  
 
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      </Head>
-      <body className="h-screen w-full relative bg-black">
-        {/* Smooth Fade for Overlay */}
-        <div
-          className={`absolute inset-0 flex items-center justify-center bg-black transition-opacity duration-700 ${
-            showOverlay ? "opacity-100" : "opacity-0 pointer-events-none"
-          }`}
-        >
-          {showOverlay && <img src="/rotate.png" alt="Rotate to continue" className="w-full h-full object-cover" />}
-        </div>
+    <body className="h-screen w-full relative bg-black">
+      {/* Smooth Fade for Overlay */}
+      <div
+        className={`absolute inset-0 flex items-center justify-center bg-black transition-opacity duration-700 ${
+          showOverlay ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      >
+        {showOverlay && <img src="/rotate.png" alt="Rotate to continue" className="w-full h-full object-cover" />}
+      </div>
 
-        {!isPortrait && children}
-      </body>
-    </>
+      {!isPortrait && children}
+    </body>
   );
 }
