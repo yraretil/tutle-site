16b0df30f55103b25502cd6c6d1de0b6bcb97d09
Sahitya Shankar <146736193+yraretil@users.noreply.github.com>
2025-02-13 13:55:12 +0530

    Update layout.tsx
    blahblah

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
       <body className="h-screen w-full relative bg-black">
         {/* Smooth Fade for Overlay */}
         <div
